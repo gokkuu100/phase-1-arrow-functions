29b68265cc673cce80cf6fad86d6e41c85fa60e6
goku <wkurts247@gmail.com>
2023-06-22 13:24:34 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function () {
+    return 2000/100;
+}
+console.log(divide());
+
+const square = (para1) => para1*para1;
+console.log(square(9));
+
+const add = (para1, para2) => para1 + para2;
+console.log(add(2, 3));
